7894b5c5189ce7a43f480b0f7466690e85db720c
sinequanonn <dleck288@gmail.com>
2026-05-09 21:02:49 +0900

    chore: 통합 테스트 환경 구성

--- backend/src/main/java/trainfocus/backend/auth/firebase/FirebaseConfig.java
+++ backend/src/main/java/trainfocus/backend/auth/firebase/FirebaseConfig.java
@@ -7,6 +7,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,6 +17,7 @@ import java.io.InputStream;
 
 @Slf4j
 @Configuration
+@ConditionalOnProperty(name = "firebase.enabled", havingValue = "true", matchIfMissing = true)
 public class FirebaseConfig {
 
     @Value("${firebase.service-account-path}")
