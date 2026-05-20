ac707e07d60165095534761893157d4999ec0c97
sinequanonn <dleck288@gmail.com>
2026-05-20 19:57:11 +0900

    feat: 프론트 모니터링 추가

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Noto_Sans_KR } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const notoSansKr = Noto_Sans_KR({
@@ -20,7 +22,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko" className={`${notoSansKr.variable} h-full antialiased`}>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+          {children}
+          <Analytics />
+          <SpeedInsights />
+        </body>
     </html>
   );
 }
